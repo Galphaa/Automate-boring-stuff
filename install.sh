410116221006bc508a216153a8bd4db158eff283
galphaa <gkeratishvili@gmail.com>
2017-05-12 16:32:08 +0400

    Update

--- install.sh
+++ install.sh
@@ -8,37 +8,15 @@ cat list.txt | xargs apt-get -y install
 
 apt-get -y update && apt-get -y upgrade 
 
-
-# Installing ATOM.
-
-cd
-mkdir .installs
-cd .installs
-wget https://atom.io/download/deb    
-dpkg -i deb
-apt-get -y update
-
-
-# Installing synergy 1.8.8 
-
-wget http://archive.getdeb.net/ubuntu/pool/apps/s/synergy/synergy_1.8.8-1~getdeb1_amd64.deb    
-dpkg -i synergy_1.8.8-1~getdeb1_amd64.deb
-apt-get -f install -y
-
-
 # Installing repo and then installing pulseaudio-equalizer for more easy control for audio system.
 
 add-apt-repository -y ppa:nilarimogard/webupd8 
 apt-get update
 apt-get install pulseaudio-equalizer
 
-# Disable Chrome annoying keyloging after this command once will be requested.
+# Putting font forrome and first time it will aske for key enterence just press enter and second time it will not aske for it.
 
-rm ~/.local/share/keyrings/*
-
-# Putting font for zshall 
-
-cp ourceCodePro+Powerline+Awesome+Regular.ttf /usr/share/fonts/truetype/
+cp SourceCodePro+Powerline+Awesome+Regular.ttf /usr/share/fonts/truetype/
 
 # Making configuration for confuguring vundell .bashrc .vimrc and tmux.config (before evry fonfiguration is needed .")
 
@@ -50,24 +28,10 @@ cp dotfiles/vimrc.txt ~/.vimrc
 cp dotfiles/tmux.conf.txt ~/.tmux.conf
 cp dotfiles/bashrc.txt ~/.bashrc
 
-# Installing Oh My Zsh
 
-apt-get install zsh
+# Installing Google chrome
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+apt-get install libxss1 libappindicator1 libindicator7
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
 
-cd ~/.oh-my-zsh/custom
-git clone https://github.com/bhilburn/powerlevel9k.git themes/powerlevel9k
-cd - 
-cp  dotfiles/zshrc ~/.zshrc
-
-# Installing Fonts
-cd ~
-git clone https://github.com/powerline/fonts
-cd fonts
-./install.sh
-
-# cleaning 
-
-cd ~ 
-rm -r fonts
