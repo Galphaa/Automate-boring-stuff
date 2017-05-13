e10ea76fbd4d017640a0b8a9c06d38e1485a36da
galphaa <gkeratishvili@gmail.com>
2017-05-13 12:59:32 +0400

    Update

--- install.sh
+++ install.sh
@@ -41,4 +41,5 @@ apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 
+clear
 cat to_do_list.txt 
