35919c6132017d978195b8c85a6319ae44829946
galphaa <gkeratishvili@gmail.com>
2017-09-15 23:55:36 +0400

    updated

--- install.sh
+++ install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash 
 
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+WORKING_DIR=`mktemp -d -p /tmp`
+
+
 # Prepearing 
 apt-get -y update  > /dev/null 2>&1
 apt-get -y upgrade > /dev/null 2>&1
 
-
-CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-WORKING_DIR=`mktemp -d -p /tmp`
+# Installing Google chrome repository
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
 
 # Downloading dotfiles
@@ -16,37 +20,25 @@ cd $CURRENT_DIR
 
 
 # Installing all files needed for first usage.
-
 cat list.txt | xargs apt-get -y install   
 apt-get -y update > /dev/null 2>&1
 apt-get -y upgrade > /dev/null 2>&1
 
 # Installing repo and then installing pulseaudio-equalizer for more easy control for audio system.
-
 add-apt-repository -y ppa:nilarimogard/webupd8 
 apt-get -y update > /dev/null 2>&1
 apt-get -y install pulseaudio-equalizer
 
 # Putting font forrome and first time it will aske for key enterence just press enter and second time it will not aske for it.
-
 cp .files/SourceCodePro+Powerline+Awesome+Regular.ttf /usr/share/fonts/truetype/
 
 # Making configuration for confuguring vundell .bashrc .vimrc and tmux.config (before evry fonfiguration is needed .")
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp $WORKING_DIR/dotfiles/plugins.vim.txt ~/.vim/bundle/plugins.vim
 cp $WORKING_DIR/dotfiles/vimrc.txt ~/.vimrc
 cp $WORKING_DIR/dotfiles/tmux.conf.txt ~/.tmux.conf
 cp $WORKING_DIR/dotfiles/bashrc.txt ~/.bashrc
 
-
-# Installing Google chrome
-
-apt-get -y install libxss1 libappindicator1 libindicator7
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get -y update 
-sudo apt-get install google-chrome-stable
 
 clear
 cat to_do_list.txt 
