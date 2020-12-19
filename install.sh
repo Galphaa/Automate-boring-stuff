57784977df05ff9efc57cb3b567c080906201d34
Giorgi Keratishvili <gkeratishvili@gmail.com>
2020-12-19 13:53:28 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,10 +26,6 @@ cd $CURRENT_DIR
 cat list.txt | xargs apt-get -y -qq install   
 update_upgrade
 
-# Installing repo and then installing pulseaudio-equalizer for more easy control for audio system.
-add-apt-repository -y ppa:nilarimogard/webupd8 
-apt-get -y -qq install pulseaudio-equalizer
-
 # Putting font forrome and first time it will aske for key enterence just press enter and second time it will not aske for it.
 cp .files/SourceCodePro+Powerline+Awesome+Regular.ttf /usr/share/fonts/truetype/
 
@@ -37,7 +33,7 @@ cp .files/SourceCodePro+Powerline+Awesome+Regular.ttf /usr/share/fonts/truetype/
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp $WORKING_DIR/dotfiles/plugins.vim.txt ~/.vim/bundle/plugins.vim
 cp $WORKING_DIR/dotfiles/vimrc.txt ~/.vimrc
-cp $WORKING_DIR/dotfiles/.tmux.conf.txt ~/.tmux.conf
+cp $WORKING_DIR/dotfiles/tmux.conf.txt ~/.tmux.conf
 cp $WORKING_DIR/dotfiles/bashrc.txt ~/.bashrc
 cp $WORKING_DIR/dotfiles/config_1 ~/.config/tilda/config_0
 
